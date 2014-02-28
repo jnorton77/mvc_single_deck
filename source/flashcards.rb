b75a5fdedbc38b107cfe7ff76bdbccc51f3a7771
Batman <bruce_wayne@devbootcamp.com>
2014-02-28 11:00:47 -0600

    skeleton

--- source/flashcards.rb
+++ source/flashcards.rb
@@ -1,1 +1,29 @@
-# Main file for 
+# MODEL - Devin
+class Card
+
+#instantiate card object
+
+end
+
+class Deck
+
+# LOAD flashcard_samples.txt
+
+end
+
+# CONTROLLER - John
+
+class Flashcards
+
+# run! method (ref to render, user_input, compare)
+
+end
+
+# VIEW - Pablo
+
+class CardView
+
+# displays card
+# def to_s
+
+end
